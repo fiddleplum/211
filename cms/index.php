d9e57ec0e0f3b9ecf918e517e793d4aa31af95da
fiddleplum <stephenhurley@gmail.com>
2016-05-13 18:33:07 -0700

    15, made cms not zoom in on mobile

--- cms/index.php
+++ cms/index.php
@@ -37,6 +37,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 <title>Content Management System</title>
 <link rel="stylesheet" type="text/css" href="cms.css"/>
 </head>
