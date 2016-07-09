68347d66cb47634980c6d9a6a7b19fe3e5c407f0
Ericka Koyama <ericka.loyama@gmail.com>
2016-07-09 07:20:44 -0700

    added foundation to app side and moved translation widget

--- js/script.js
+++ js/script.js
@@ -264,7 +264,7 @@ function updateList() {
 
 $(document).ready(function(){
   $('#google_translate_element').bind('DOMNodeInserted', function(event) {
-    $('.goog-te-combo option:first').html('En');
+    $('.goog-te-combo option:first').html('English');
     $('.goog-te-menu-frame.skiptranslate').load(function(){
       setTimeout(function(){
         $('.goog-te-menu-frame.skiptranslate').contents().find('.goog-te-menu2-item-selected .text').html('Translate');
